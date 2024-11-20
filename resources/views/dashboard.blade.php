8cd4851cc2cf44e6eb7d2bbd401421782c1f4df7
caramelloj <caramellojose54@gmail.com>
2024-11-20 20:24:15 -0300

    added login/register view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,12 +7,12 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+
 @stop
 
 @section('css')
     {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+    <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
